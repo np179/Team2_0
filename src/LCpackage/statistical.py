b5ebc8860f983e236db43a93d2c765c76b33e3e9
np179 <christian.delavier@gmail.com>
2021-03-21 22:08:25 +0100

    updated some functions

--- src/LCpackage/statistical.py
+++ src/LCpackage/statistical.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sn
+from pathlib import Path
 
 def read_in_df(filedir, filename):
     """[Read a .csv-type File to a DataFrame]
@@ -34,3 +34,53 @@ def del_below(df,threshhold):
             dellist.append(i)               #add their index to the list
     df.drop(df.columns[dellist], axis=1, inplace=True)      #delete the columns
     return df        
+
+def plot_and_save_multiple(df, xaxis, var_name, value_name, path):
+    """[Plot multiple columns of a DataFrame as lines against one specified column and save the plot in a designated location]
+
+     Args:
+        df ([DataFrame]): [Input DataFrame] \n
+        xaxis ([String or Index]): [Designates Column of variable to plot against] \n
+        var_name ([String]): [Describes the Columns plotted] \n
+        value_name ([String]): [Description of the y-axis] \n
+        path ([String]): [Designates location to save plot to]
+    """
+    df_2_melted=df_2.melt(xaxis, var_name=var_name, value_name=value_name)
+    sn.relplot(data=df_2_melted, kind='line', x=xaxis, y=value_name, hue=var_name)  #write routine to check if file exists later
+    plt.show()
+    if Path(path).exists() == False:   
+        plt.savefig(path)
+    elif Path(path).exists() == True:
+        print('{} already exists. Overwrite? Y/N'.format(path))
+        check = input()
+        if check == 'Y':
+            plt.savefig(path)
+            print('File {} was overwriten'.format(path))
+        else:
+            print('File {} was not overwriten'.format(path))
+    
+    if Path(path)
+    plt.savefig(path)
+
+def plot_and_save_single(df, xaxis, yaxis, path):    
+    """[Plot a single column of a DataFrame as lines against one specified column and save the plot in a designated location]
+
+    Args:
+        df ([DataFrame]): [Input DataFrame] \n
+        xaxis ([String or Index]): [Designates column to plot against] \n
+        yaxis ([String or Index]): [Designates column to plot] \n
+        path ([String]): [Designates location to save plot to] \n
+    """
+    sn.relplot(data=df, kind='line', x=xaxis, y=yaxis)   
+    plt.savefig(path)
+    plt.show()
+    if Path(path).exists() == False:   
+        plt.savefig(path)
+    elif Path(path).exists() == True:
+        print('{} already exists. Overwrite? Y/N'.format(path))
+        check = input()
+        if check == 'Y':
+            plt.savefig(path)
+            print('File {} was overwriten'.format(path))
+        else:
+            print('File {} was not overwriten'.format(path))
