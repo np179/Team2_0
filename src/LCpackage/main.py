fe4472a01b974a0e5be75fed6ea8e81fb6007bb9
John Doe <W>
2021-03-23 08:45:37 +0100

    some documentation is still missing

---
+++ src/LCpackage/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import numerical as nu
+
+'''test'''
+#x = np.array([[3, 0, 0], [0, 4, 0], [5, 6, 0]])
+#test1 = nu.read_in_np( 'data/' , 'efield.t' )
+#print(test1)
+
+#test2 = nu.check_significant_np(test1, 0)
+#print(test2)
